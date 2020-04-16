8a764b1189cb7fdaf9f4a577b784ac434867dcf6
Cijan  <cijan@CijandeMBP.lan>
2020-04-16 18:11:17 +0800

    Site updated: 2020-04-16 18:11:17

--- js/pikaday.js
+++ js/pikaday.js
@@ -22,7 +22,7 @@
       return [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
     },
     Pikaday = function (opts) {
-      var data = '{"2020":{"2":{"21":true},"3":{"12":true}}}';
+      var data = '{"2020":{"2":{"21":true}}}';
       this.data = JSON.parse(data);
       window.pdata = this.data;
       this.weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
